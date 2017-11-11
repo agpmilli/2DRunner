22e5b5f2f18d441859cccc3aed16759a5d566366
alexcrt <alexis.cartier@gmail.com>
2017-11-11 16:20:58 +0100

    raph pd

--- client/js/main.js
+++ client/js/main.js
@@ -28,7 +28,7 @@ var bird = {
 
 		// update y position
 		this.yVelocity = this.yVelocity + this.yAcceleration;
-		this.y = min(canvasHeight-birdHeight, this.y+this.yVelocity);
+		this.y = min(this.ground(this.x), this.y + this.yVelocity);
 
 		//reset jump count when you touch the ground
 		if(this.isDown()){
@@ -42,18 +42,45 @@ var bird = {
 	isRightMost: function(){
 		return bird.x + bird.width >= canvasWidth -sideMargin;
 	},
+	ground: function(x) {
+		for(var i = 0; i < blocks.length; i++) {
+			block = blocks[i];
+			if(bird.x + birdWidth / 2 >= block.x && bird.x <= (block.x + block.width) && (bird.y - birdHeight) <= block.y) {
+				return block.y - birdHeight;
+			}
+		}
+		return canvasHeight-birdHeight;
+	},
 	isDown: function(){
-		return bird.y + birdHeight >= canvasHeight;
+		return this.y + birdHeight >= this.ground(this.x);
 	}
 }
 
 //p5js functions
 var image_bird_left;
 var image_bird_right;
+var block_tile;
+
+var blocks = [Block(300, 250), Block(329, 250), Block(358, 250)];
+
+function Block(x, y) {
+	return {
+		width: 30,
+		height: 30,
+		x: x,
+		y: y,
+		draw : function(){
+			image(block_tile, this.x, this.y, this.width, this.height);
+		}
+	}
+}
+
 function preload(){
 	image_bird_left = loadImage("images/bird_left.png")
 	image_bird_right = loadImage("images/bird_right.png")
+	block_tile = loadImage("images/block.png")
 }
+
 function setup(){
 	createCanvas(canvasWidth, canvasHeight);
 }
@@ -67,11 +94,16 @@ function draw(){
 	else if(keyIsDown(LEFT_ARROW)){
 		bird.xVelocity = -5;
 	}
-	else{
+	else {
 		bird.xVelocity = 0;
 	}
 	bird.update();
 	bird.draw();
+
+	blocks.forEach(function(block) {
+		block.draw();
+	});
+
 	fill("#FFF");
 	text("score : " + score, 10, 10);
 
@@ -81,7 +113,6 @@ function draw(){
 		text("GAME OVER", 180, 200)
 	}
 }
-
 // on press space bar, jump
 function keyTyped(){
 	if(keyCode == 32 && bird.jumpCount < MAX_JUMPS){
