e3b9d92663c0bd24735c9ecb294051cba4803f91
agpmilli <alain.milliet@epfl.ch>
2017-11-11 14:05:10 +0100

    shared canvas server

--- server/public/sketch.js
+++ server/public/sketch.js
@@ -1,81 +1,59 @@
-// Daniel Shiffman
-// http://codingtra.in
-// http://patreon.com/codingtrain
-// Code for: https://youtu.be/ZjVyKXp9hec
-
-// Keep track of our socket connection
 var socket;
-
-var blob;
-
-var blobs = [];
-var zoom = 1;
+var lineThickness;
+var lineColor;
 
 function setup() {
-  createCanvas(600, 600);
-  // Start a socket connection to the server
-  // Some day we would run this server somewhere else
-  socket = io.connect('http://localhost:3000');
+    var canv = createCanvas(windowWidth, windowHeight);
+    canv.parent('canvas');
+    background(255,255,255);
+
+    lineColor = [random(1,255), random(1,255), random(1,255)];
+
+    socket = io.connect('http://128.179.131.152:3000');
+    socket.on('mouse', newDrawing);
+
+    rSlider = createSlider(0, 255, lineColor[0]);
+    rSlider.parent('colors');
+    gSlider = createSlider(0, 255, lineColor[1]);
+    gSlider.parent('colors');
+    bSlider = createSlider(0, 255, lineColor[2]);
+    bSlider.parent('colors');
+
+    tSlider = createSlider(5, 40, 20);
+    tSlider.parent('thickness');
+
+}
+
+function newDrawing(data) {
+    noStroke();
+    fill(data.color[0], data.color[1], data.color[2]);
+    ellipse(data.x, data.y, data.thickness, data.thickness);
+}
+
+function mouseDragged() {
+
+    var data = {
+        x: mouseX,
+        y: mouseY,
+        color: lineColor,
+        thickness: lineThickness
+    };
+
+    socket.emit('mouse', data);
+
+    lineThickness = tSlider.value();
+    lineColor = [rSlider.value(), gSlider.value(), bSlider.value()];
+
+    noStroke();
+    fill(lineColor[0], lineColor[1], lineColor[2]);
+    ellipse(mouseX, mouseY, lineThickness, lineThickness);
 
 
-  blob = new Blob(random(width), random(height), random(8, 24));
-  // Make a little object with  and y
-  var data = {
-    x: blob.pos.x,
-    y: blob.pos.y,
-    r: blob.r
-  };
-  socket.emit('start', data);
-
-  socket.on('heartbeat',
-    function(data) {
-      //console.log(data);
-      blobs = data;
-    }
-  );
 }
 
 function draw() {
-  background(0);
-  console.log(blob.pos.x, blob.pos.y);
-
-  translate(width / 2, height / 2);
-  var newzoom = 64 / blob.r;
-  zoom = lerp(zoom, newzoom, 0.1);
-  scale(zoom);
-  translate(-blob.pos.x, -blob.pos.y);
-
-  for (var i = blobs.length - 1; i >= 0; i--) {
-    var id = blobs[i].id;
-    if (id.substring(2, id.length) !== socket.id) {
-      fill(0, 0, 255);
-      ellipse(blobs[i].x, blobs[i].y, blobs[i].r * 2, blobs[i].r * 2);
-
-      fill(255);
-      textAlign(CENTER);
-      textSize(4);
-      text(blobs[i].id, blobs[i].x, blobs[i].y + blobs[i].r);
+    document.getElementById("brush-color").style.backgroundColor = "rgb("+lineColor[0]+","+lineColor[1]+","+lineColor[2]+")";
+    if (mouseIsPressed) {
+        mouseDragged();
     }
-    // blobs[i].show();
-    // if (blob.eats(blobs[i])) {
-    //   blobs.splice(i, 1);
-    // }
-  }
-
-
-
-  blob.show();
-  if (mouseIsPressed) {
-    blob.update();
-  }
-  blob.constrain();
-
-  var data = {
-    x: blob.pos.x,
-    y: blob.pos.y,
-    r: blob.r
-  };
-  socket.emit('update', data);
-
-
 }
