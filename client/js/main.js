bd290786a3765bb3531792500aeaab03db80d4a4
rbsteinm <rbsteinm@gmail.com>
2017-11-11 14:16:53 +0100

    push bird movements

--- client/js/main.js
+++ client/js/main.js
@@ -1,5 +1,6 @@
 var canvasHeight = 400;
 var canvasWidth = 600;
+const sideMargin = 30;
 var score = 0;
 //bird
 var bird = {
@@ -7,55 +8,19 @@ var bird = {
 	y: 190,
 	width: 40,
 	height: 40,
-	yVelocity : 0,
-	yAcceleration: -1.7,
+	xVelocity: 0,
 	draw : function(){
 		image(image_bird, this.x, this.y, this.width, this.height);
 	},
 	update : function(){
-		this.yVelocity = this.yVelocity + 0.5*this.yAcceleration;
-		var tempy = this.y - this.yVelocity;
-		if(tempy<0)
-			this.y = 0;
-		else if(tempy > canvasHeight - this.height)
-			this.y = canvasHeight - this.height;
-		else
-			this.y -= this.yVelocity;
+		bird.x += bird.xVelocity;
+	},
+	isDead: function(){
+		return bird.x <= 0;
+	},
+	isRightMost: function(){
+		return bird.x + bird.width >= canvasWidth -sideMargin;
 	}
-}
-
-///obstacles
-var obstacles = [];
-
-function Obstacle(I){
-	I.active = true;
-	I.x = canvasWidth;
-	I.xVelocity = 2;
-	I.slitHeight = 150;
-	I.width = 20;
-	I.slit = Math.random() * (canvasHeight - I.slitHeight);
-	I.inBounds = function(){
-		return I.x +I.width;
-	}
-	I.update = function(){
-		I.active = I.active && I.inBounds();
-		I.x -= I.xVelocity;
-	};
-	I.draw = function(){
-		rect(I.x, 0, I.width, I.slit);
-		rect(I.x, I.slit + I.slitHeight, I.width, canvasHeight - (I.slit + I.slitHeight) );
-	};
-	return I;
-}
-//aut-generate obstacles
-setInterval(function(){
-	obstacles.push(Obstacle({}));
-	score++;
-}, 2000);
-//collision detection function
-function collision(bird, obstacle){
-	return (bird.y <= obstacle.slit || bird.y + bird.width >= obstacle.slit + obstacle.slitHeight) && 
-		bird.x + bird.width >= obstacle.x && bird.x <= obstacle.x + obstacle.width;
 }
 
 //p5js functions
@@ -69,26 +34,23 @@ function setup(){
 function draw(){
 	clear();
 	background(45);
-	if(keyIsDown(32)){
-		bird.yVelocity = 10;
+	if(keyIsDown(68) && !bird.isRightMost()){
+		bird.xVelocity = 5;
+	}
+	else if(keyIsDown(65)){
+		bird.xVelocity = -5;
+	}
+	else{
+		bird.xVelocity = 0;
 	}
 	bird.update();
 	bird.draw();
 	fill("#FFF");
 	text("score : " + score, 10, 10);
-	obstacles = obstacles.filter(function(obstacle){
-		return obstacle.active;
-	});
-	obstacles.forEach(function(obstacle){
-		obstacle.update();
-		obstacle.draw();
-	});
 
-	obstacles.forEach(function(obstacle){
-		if(collision(bird, obstacle)){
-			noLoop();
-			textSize(40);
-			text("GAME OVER", 180, 200);
-		}
-	});
+	if(bird.isDead()){
+		noLoop();
+		textSize(40);
+		text("GAME OVER", 180, 200)
+	}
 }
