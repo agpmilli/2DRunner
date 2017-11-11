84f982cf334ea005eb85e8a0051c1eed48a08c71
rbsteinm <rbsteinm@gmail.com>
2017-11-11 15:45:06 +0100

    gravity and double jump and alexis wasting my time

--- client/js/main.js
+++ client/js/main.js
@@ -2,13 +2,19 @@ var canvasHeight = 400;
 var canvasWidth = 600;
 const sideMargin = 30;
 var score = 0;
+const birdHeight = 40;
+const birdWidth = 40;
+const MAX_JUMPS = 2;
 //bird
 var bird = {
+	jumpCount: 0,
 	x: 100,
-	y: 190,
+	y: canvasHeight - birdHeight,
 	width: 40,
 	height: 40,
 	xVelocity: 0,
+	yVelocity : 0,
+	yAcceleration: 1.7,
 	draw : function(){
 		var tile = image_bird_right;
 		if(bird.xVelocity < 0) {
@@ -17,13 +23,27 @@ var bird = {
 		image(tile, this.x, this.y, this.width, this.height);
 	},
 	update : function(){
-		bird.x += bird.xVelocity;
+		//update x position
+		this.x += this.xVelocity;
+
+		// update y position
+		this.yVelocity = this.yVelocity + this.yAcceleration;
+		this.y = min(canvasHeight-birdHeight, this.y+this.yVelocity);
+
+		//reset jump count when you touch the ground
+		if(this.isDown()){
+			this.jumpCount = 0;
+		}
+
 	},
 	isDead: function(){
 		return bird.x <= 0;
 	},
 	isRightMost: function(){
 		return bird.x + bird.width >= canvasWidth -sideMargin;
+	},
+	isDown: function(){
+		return bird.y + birdHeight >= canvasHeight;
 	}
 }
 
@@ -40,10 +60,11 @@ function setup(){
 function draw(){
 	clear();
 	background(45);
-	if(keyIsDown(68) && !bird.isRightMost()){
+
+	if(keyIsDown(RIGHT_ARROW) && !bird.isRightMost()){
 		bird.xVelocity = 10;
 	}
-	else if(keyIsDown(65)){
+	else if(keyIsDown(LEFT_ARROW)){
 		bird.xVelocity = -5;
 	}
 	else{
@@ -58,5 +79,13 @@ function draw(){
 		noLoop();
 		textSize(40);
 		text("GAME OVER", 180, 200)
+	}
+}
+
+// on press space bar, jump
+function keyTyped(){
+	if(keyCode == 32 && bird.jumpCount < MAX_JUMPS){
+		bird.yVelocity = -20;
+		bird.jumpCount += 1;
 	}
 }
